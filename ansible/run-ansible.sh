99bff47a794be599d2eb0ca8b2f5c69643d3d1a2
Andrade Sampaio <Andrade Sampaio>
2020-06-04 11:52:58 -0300

    Criando book doansible e fazendo o deploy da aplicação

--- ansible/run-ansible.sh
+++ ansible/run-ansible.sh
@@ -2,6 +2,5 @@
 
 echo "Starting ansible..."
 
-ANSIBLE_HOST_KEY_CHECKING=false
-ansible-playbook -i ../terraform/hosts --private-key ../terraform/key/beerstore_key beerstore-playbook.yml -v
+ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook -i ../terraform/hosts --private-key ../terraform/key/beerstore_key beerstore-playbook.yml -v
 
